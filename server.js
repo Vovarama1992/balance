480bccfac7b0a530368339d0a3ccf4b10dc09d89
Vovarama1992 <vovayhh9988@gmail.com>
2024-07-24 09:05:20 +0300

    readme

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import userRoutes from './routes/userRoutes.js';
 import db from './models/index.js';
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
